049928f4c2f81232313796e1577ec2d5e260c0f2
Pachi-Dev <pachi.claros@gmail.com>
2024-02-11 05:54:27 -0400

    Agrega imágenes y actualiza el diseño de la página

---
+++ src/contans_logos.js
@@ -0,0 +1,24 @@
+const strategic_allies = [
+    { link: '', src: '/img/CONGRESO-VITIVINICOLA.webp', alt:'Congreso vitivinicola', width: 200, height: 127 },
+    { link: 'https://cevit.cetys.mx/', src: '/img/CEVIT.webp', alt:'centro de estudios vitivinicola', width: 250, height: 60 },    
+    { link: '', src: '/img/AUVG.webp', alt:'Congreso vitivinicola', width: 150  , height: 94},
+    { link: 'https://guanajuato.mx/ruta-del-vino/', src: '/img/Gobierno_de_Guanajuato.webp', alt:'Gobierno de Guanajuato', width: 250, height: 67 },
+]
+
+const media_partners = [
+    { link: 'https://www.multimediostv.com/', src: '/img/Multimedios_Logo.webp', alt:'Multimedios', width: 250 },
+    { link: 'https://www.chicmagazine.com.mx/', src: '/img/LOGO-CHIC.webp', alt:'Chic Magazine', width: 200 },
+    { link: 'https://www.milenio.com/', src: '/img/Logo-Grupo-Milenio.webp', alt:'Grupo Milenio', width: 200 },
+    { link: 'https://tvcuatro.tv/', src: '/img/TV-cuatro.webp', alt:'TV cuatro', width: 100 },
+    { link: 'https://www.am.com.mx/', src: '/img/am.webp', alt:'AM', width: 200 },
+    { link: '', src: '/img/Blu-923.webp', alt:'BLU 92.3', width: 200 },
+    { link: '', src: '/img/U-983.webp', alt:'U 98.3', width: 200 },
+    { link: 'https://www.elsoldelbajio.com.mx/', src: '/img/crinado.webp', alt:'Crinado', width: 150 },
+    { link: 'https://www.am.com.mx/temas/revista-cerro-gordo-12675.html?true', src: '/img/Cerro-Gordo_logo.webp', alt:'Cerro Gordo', width: 150 },
+    { link: 'https://www.elsoldeleon.com.mx/', src: '/img/El-Sol-de-Leon.webp', alt:'El sol de Leon', width: 250 },
+    { link: 'https://heraldodemexico.com.mx/', src: '/img/heraldoHorizontal.webp', alt:'Heraldo Media Group', width: 250 },
+    { link: 'https://www.globalindustrial.com/', src: '/img/logo-global-industries.webp', alt:'global industries', width: 250 },
+    { link: 'https://www.publimetro.com.mx/', src: '/img/LOGO-PUBLIMETRO.webp', alt:'Publimetro', width: 250 },
+    { link: 'https://www.radioformula.com.mx/bajio/', src: '/img/radio-formula.webp', alt:'Radio Formula', width: 200 }
+]
+export { strategic_allies, media_partners}
